43b6e6f538b7cca727490e2d3b8bbc7da5390a7b
Viktor Lukyanchenkov <viktor.lukyanchenkov@cd.p2h.com>
2022-11-29 09:54:33 +0200

    04 create and work with modules

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const CC = require('currency-converter-lt')
-let currencyConverter = new CC({from:"USD", to:"EUR", amount:100})
+const os = require('os')
+let res = os.platform()
+console.log(res) // win32
 
-currencyConverter.convert().then((response) => {
-  console.log(`Result: ${response}`)
-})
+const my_math = require('./modules/my_math')
+let res1 = my_math.add(4, 5)
+let res2 = my_math.add(3, 4)
+console.log(res1)
+console.log(res2)
